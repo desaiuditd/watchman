64be5c4634b81d11d8f57bdf5914fe4f97b09e8c
Udit Desai <desaiuditd@gmail.com>
2014-12-11 22:20:09 +0530

    Settings Page added. Post Revision Limit added

--- settings/class-wm-settings.php
+++ settings/class-wm-settings.php
@@ -17,11 +17,65 @@ if ( ! class_exists( 'WM_Settings' ) ) {
 
 		static $page_slug = 'watchman';
 
+		static $revision_limit_key = 'wm_revision_limit_';
+
 		function __construct() {
 			add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
 			add_action( 'admin_init', array( $this, 'settings_init' ) );
 			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts_styles' ) );
 			add_action( 'admin_enqueue_scripts', array( $this, 'admin_menu_css' ) );
+			add_action( 'admin_notices', array( $this, 'display_admin_notices' ) );
+
+			add_filter( 'wp_revisions_to_keep', array( $this, 'filter_revisions_to_keep' ), 999, 2 );
+		}
+
+		function filter_revisions_to_keep( $num, $post ) {
+
+			$post_type = get_post_type( $post );
+
+			$revision = get_option( self::$revision_limit_key . $post_type, false );
+
+			if ( empty( $revision ) || ! is_numeric( $revision ) ) {
+				$num = true;
+			} else {
+				$num = intval( $revision );
+			}
+
+			return $num;
+		}
+
+		function show_message( $message, $type = 'info' ) { ?>
+			<div class="<?php echo $type; ?>"><?php echo $message; ?></div>
+		<?php }
+
+		function display_admin_notices() {
+			// check for our settings page - need this in conditional further down
+			global $current_screen;
+			if ( 'toplevel_page_'.self::$page_slug != $current_screen->id ) {
+				return;
+			}
+
+			$wm_settings_pg = strpos( $_GET['page'], self::$page_slug );
+			// collect setting errors/notices: //http://codex.wordpress.org/Function_Reference/get_settings_errors
+			$set_errors = get_settings_errors();
+
+			//display admin message only for the admin to see, only on our settings page and only when setting errors/notices are returned!
+			if ( current_user_can( 'manage_options' ) && false !== $wm_settings_pg && ! empty( $set_errors ) ) {
+
+				// have our settings succesfully been updated?
+				if ( 'settings_updated' === $set_errors[0]['code'] && isset( $_GET['settings-updated'] ) ) {
+
+					$this->show_message( '<p>' . $set_errors[0]['message'] . '</p>', 'updated' );
+
+					// have errors been found?
+				} else {
+					// there maybe more than one so run a foreach loop.
+					foreach ( $set_errors as $set_error ) {
+						// set the title attribute to match the error "setting title" - need this in js file
+						$this->show_message( '<p>' . $set_error['message'] . '</p>', 'error' );
+					}
+				}
+			}
 		}
 
 		function enqueue_scripts_styles() {
@@ -43,6 +97,18 @@ if ( ! class_exists( 'WM_Settings' ) ) {
 					content: 'a';
 					padding: 0 8px 0 0;
 				}
+
+				.wm-icon {
+					margin-top: -8px;
+					margin-right: 5px;
+					float: left;
+				}
+				.wm-icon:before {
+					font-size: 35px;
+				}
+				h2.wm-heading {
+					line-height: 22px;
+				}
 			";
 			wp_add_inline_style( 'wp-admin', $css );
 		}
@@ -52,34 +118,49 @@ if ( ! class_exists( 'WM_Settings' ) ) {
 		}
 
 		function settings_init() {
-			register_setting( 'watchman_group', 'watchman_settings' );
 
-			add_settings_section( 'wm_revision_limit_section', __( 'Revision Limits', WM_TEXT_DOMAIN ), array( $this, 'revision_limit_section_callback' ), 'watchman_group' );
+			add_settings_section( self::$revision_limit_key . 'section', __( 'Revision Limits', WM_TEXT_DOMAIN ), array( $this, 'revision_limit_section_callback' ), 'watchman_group' );
+
+			$post_types = get_post_types( array(), 'objects' );
+			foreach ( $post_types as $pt ) {
+				$does_support = post_type_supports( $pt->name, 'revisions' );
+				if ( $does_support ) {
+					add_settings_field( self::$revision_limit_key . $pt->name, $pt->labels->name, array( $this, 'revision_limit_callback' ), 'watchman_group', self::$revision_limit_key . 'section', array( 'post_type' => $pt->name ) );
+					register_setting( 'watchman_group', self::$revision_limit_key . $pt->name );
+					add_filter( 'sanitize_option_' . self::$revision_limit_key . $pt->name, array( $this, 'sanitize_revision_limits' ), 10, 2 );
+				}
+			}
 		}
 
-		function revision_limit_section_callback() {
-			_e( 'This section lets you control revision limits on your post types.', WM_TEXT_DOMAIN );
+		function sanitize_revision_limits( $value, $option ) {
+
+			$post_type = str_replace( self::$revision_limit_key, '', $option );
+			$post_type_obj = get_post_type_object( $post_type );
+			$labels = get_post_type_labels( $post_type_obj );
+
+			if ( ! is_numeric( $value ) ) {
+				add_settings_error( $option, 'watchman_errors', sprintf( __( 'You need to fill numeric value for %s revision limit.', WM_TEXT_DOMAIN ), '<strong>' . $labels->name . '</strong>' ) );
+			} else {
+				$value = absint( $value );
+			}
+
+			return $value;
 		}
+
+		function revision_limit_section_callback() { ?>
+			<p class="description">
+				<?php _e( 'This section lets you control revision limits on your post types. Leave blank in case of unlimited revisions.', WM_TEXT_DOMAIN ); ?>
+			</p>
+		<?php }
+
+		function revision_limit_callback( $args ) { ?>
+			<input type="text" name="<?php echo self::$revision_limit_key . $args['post_type']; ?>" id="<?php echo self::$revision_limit_key . $args['post_type']; ?>" value="<?php echo get_option( self::$revision_limit_key . $args['post_type'] ); ?>" />
+		<?php }
 
 		function render_settings_page() { ?>
-			<style>
-				.wm-icon {
-					margin-top: -8px;
-					margin-right: 5px;
-					float: left;
-				}
-
-				.wm-icon:before {
-					font-size: 35px;
-				}
-
-				.wm-heading {
-					margin-top: 30px;
-				}
-			</style>
 			<div class="wrap">
 
-				<h1 class="wm-heading"><i class="wm-icon icon-watchman"></i><?php _e( 'Watchman' ); ?></h1>
+				<h2 class="wm-heading"><i class="wm-icon icon-watchman"></i><?php _e( 'Watchman' ); ?></h2>
 
 				<form action='options.php' method='post'>
 
